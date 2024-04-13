304136e179605d1570fd95730d9ecf587245b982
Rafi Ersözlü <ersozlu.rafi@gmail.com>
2024-04-13 21:18:59 +0300

    add: qrcode

--- src/components/ConnectModal.tsx
+++ src/components/ConnectModal.tsx
@@ -1,3 +1,4 @@
+import { QRCode } from "react-qrcode-logo";
 import { useClaveWallet } from "../context";
 
 const ConnectModal = () => {
@@ -8,11 +9,22 @@ const ConnectModal = () => {
         <div className="modalHeader">
           <img className="partnerLogo" src={claveConnector.dappInfo?.image} alt="dappImage" />
           <p>{claveConnector.dappInfo?.name}</p>
-          <p className="closeButton">x</p>
+          <p className="closeButton">✖</p>
+        </div>
+        <div className="qrWrapper">
+          <QRCode
+            value={claveConnector.connectionId}
+            size={280}
+            bgColor="transparent"
+            qrStyle="dots"
+            eyeColor="#E5E7EB"
+            fgColor="#E5E7EB"
+            eyeRadius={8}
+          />
         </div>
         <div className="modalFooter">
           <button className="openAppButton">Open in Clave app</button>
-          <p>Don't have Clave?</p>
+          <button className="dontHaveClaveButton">Don't have Clave?</button>
         </div>
       </div>
     </div>
